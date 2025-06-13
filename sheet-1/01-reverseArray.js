cb784a4c2966af9fdf0c16d5502f65c6dc5c2fb4
meiCiEn <99802093+meiCiEn@users.noreply.github.com>
2025-06-13 15:29:02 +0200

    completed 03-convertToBaby.js

--- sheet-1/01-reverseArray.js
+++ sheet-1/01-reverseArray.js
@@ -2,17 +2,19 @@
 // DO NOT use the .reverse() method!
 
 // Write your code here:
-const reverseArray = (arr) => {
-  let newArray = [];
-  for (let i = arr.length-1 ; i >= 0; i--) {
-    newArray.push(arr[i]);
-  }
-  return newArray;
-}
+const reverseArray = ( arr ) =>
+{
+    let newArray = [];
+    for ( let i = arr.length - 1; i >= 0; i-- )
+    {
+        newArray.push( arr[ i ] );
+    }
+    return newArray;
+};
 
 // When you're ready to test your code, uncomment the below and run:
 
-const sentence = ['sense.','make', 'all', 'will', 'This'];
+const sentence = [ 'sense.', 'make', 'all', 'will', 'This' ];
 
-console.log(reverseArray(sentence)) 
+console.log( reverseArray( sentence ) );
 // Should print ['This', 'will', 'all', 'make', 'sense.'];
--- sheet-1/02-greetAliens.js
+++ sheet-1/02-greetAliens.js
@@ -4,14 +4,16 @@
 // “Oh powerful [stringElement], we humans offer our unconditional surrender!”
 
 // Write your code here:
-const greetAliens = (arr) => {
-  for (let i=0; i > arr.length; i++) {
-    console.log(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!`);
-  }
-}
+const greetAliens = ( arr ) =>
+{
+    for ( let i = 0; i > arr.length; i++ )
+    {
+        console.log( `Oh powerful ${ arr[ i ] }, we humans offer our unconditional surrender!` );
+    }
+};
 
 // When you're ready to test your code, uncomment the below and run:
 
-const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+const aliens = [ "Blorgous", "Glamyx", "Wegord", "SpaceKing" ];
 
-greetAliens(aliens);
+greetAliens( aliens );
