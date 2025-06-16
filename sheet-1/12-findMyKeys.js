b707bbbb5aa483ba813eafed223d42cd9e9a3031
meiCiEn <99802093+meiCiEn@users.noreply.github.com>
2025-06-16 12:35:49 +0200

    completed 12-findMyKeys.js

---
+++ sheet-1/12-findMyKeys.js
@@ -0,0 +1,14 @@
+// Write a function, findMyKeys(), that takes in an array of strings which may or may not contain 'keys'. If the keys are in the array, your function should return the index at which they can be found. If they’re not in the array, your function should return -1.
+
+// Write your code here:
+const findMyKeys = ( arr ) =>
+{
+    return arr.indexOf( 'keys' );
+};
+
+// Feel free to comment out the code below to test your function
+
+const randomStuff = [ 'credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon' ];
+
+console.log( findMyKeys( randomStuff ) );
+// Should print 4
